db8a7a5b740376de5606451b9da17aabcb25d368
osman <osman.mohammed@adamo.es>
2021-02-12 16:07:20 +0100

    initial commit

---
+++ myflask1.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#dummy data
+blogposts = [
+	{
+		"author": "Christopher",
+		"title" : "Blog post 1",
+		"content" : "First post content",
+		"date_posted": "20th Jan 2021"
+
+	},
+	{
+		"author": "Jhon",
+		"title" : "Blog post 2",
+		"content" : "First post content",
+		"date_posted": "25th Jan 2021"
+
+	},
+
+]
+@app.route('/')
+def hello_world():
+    return render_template("home.html", blogposts=blogposts)
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title = "About")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)  #run flask in debug mode
